a09a31ab28eb3f1c2cb289b17bbea5ec4f753a5a
kexue <xana278@qq.com>
2022-04-26 22:56:34 +0800

    ✅ 添加测试

--- test/md.py
+++ test/md.py
@@ -1,3 +1,5 @@
+import io
+
 from PIL import Image
 from httpx import get
 
@@ -5,5 +7,5 @@ param = {"md": "## Hello World\n\nThis is a test."}
 
 res = get("http://127.0.0.1:8080/md/", params=param)
 
-im = Image.open(res.content)
-im.save("test.png")
+im = Image.open(io.BytesIO(res.content))
+im.save("test/test.png")
